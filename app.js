8f14bf31e18576ddb611ee139d0369bfe0489c3e
phulinMN <pongpaiMN@gmail.com>
2018-03-02 13:50:24 +0700

    heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var expressValidator = require('express-validator');
 var mongojs = require('mongojs');
 // var db = mongojs('portdb', ['persons','activitydb']);
-var db = mongojs(process.env.MONGODB_URI || 'portdb', ['persons','activitydb']);
+var db = mongojs(process.env.MONGODB_URI || 'portdb', ['persons','activitydb'], { ssl: true});
 var ObjectId = mongojs.ObjectId;
 
 var app = express();
